ff7d6a8df1fbacac93cf439fe94755fcf2b6f183
Colin Shaw <colin.shaw@aya.yale.edu>
2017-08-10 13:35:47 -0500

    Adding a video processor that uses a pre-trained model

---
+++ segment_video.py
@@ -0,0 +1,83 @@
+import numpy as np
+import scipy.misc
+import tensorflow as tf
+from tqdm import tqdm 
+from moviepy.editor import *
+
+
+class SegmentVideo(object):
+
+    '''
+    Constants
+    '''
+    image_shape = (160, 576)
+
+
+    '''
+    Constructor with param setting
+    '''
+    def __init__(self, params):
+        for p in params:
+            setattr(self, p, params[p])
+
+
+    '''
+    Segments the image
+    '''
+    def segment_image(self, image):
+        image        = scipy.misc.imresize(image, self.image_shape)
+        im_softmax   = self.sess.run([tf.nn.softmax(logits)], {keep_prob: 1.0, image_pl: [image]})
+        im_softmax   = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
+        segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
+        mask         = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
+        mask         = scipy.misc.toimage(mask, mode="RGBA")
+        street_im    = scipy.misc.toimage(image)
+        street_im.paste(mask, box=None, mask=mask)
+        as_np_array  = np.array(street_im)
+        return as_np_array
+
+
+    '''
+    Main processing loop
+    '''
+    def process_video(self):
+        new_frames = []
+        video      = VideoFileClip(self.input_video)
+        for frame in video.iter_frames():
+            new_frame = self.segment_image(frame)
+            new_frames.append(new_frame)
+        new_video = ImageSequenceClip(new_frames, fps=video.fps)
+        new_video.write_videofile(self.output_video, audio=False)
+
+
+    '''
+    Restore model and retrieve pertinent tensors
+    '''
+    def restore_model(self):
+        saver = tf.train.import_meta_graph('data/model.meta')
+        saver.restore(self.sess, tf.train.latest_checkpoint('data/'))
+        graph = tf.get_default_graph()
+        self.image_pl = graph.get_tensor_by_name('image_input:0')
+        self.logits   = graph.get_tensor_by_name('logits:0')
+
+
+    '''
+    Run the segmentation
+    '''
+    def run(self):
+        self.sess = tf.Session() 
+        self.restore_model()
+        self.process_video()
+
+
+
+'''
+Entry point
+'''
+params = {
+    'input_video':  'something',
+    'output_video': 'something'
+}
+sv = new SegmentVideo(params)
+sv.process_video()
+
